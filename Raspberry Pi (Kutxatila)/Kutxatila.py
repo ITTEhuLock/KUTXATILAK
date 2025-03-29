a7454a3eb206b92d9972e20680395e4ba4fb309b
ggonzalez088 <ggonzalez088@ikasle.ehu.eus>
2025-03-29 20:59:06 +0100

    Raspberry LED

--- Raspberry Pi (Kutxatila)/Kutxatila.py
+++ Raspberry Pi (Kutxatila)/Kutxatila.py
@@ -1,7 +1,10 @@
-import logging, requests, json, gpiozero, time
+import logging, requests, json, time
+import RPi.GPIO as GPIO
 from PN532 import PN532
 
 logging.basicConfig(level=logging.DEBUG)
+ 
+
 
 def callbackPN532(tag, id):
     print('Found tag: {}, id: {}'.format(tag, id))
@@ -10,9 +13,10 @@ def main():
     URL = "http://192.168.137.1:3000/raspberry/check"
     #Mugikorraren aplikazioa NFC eskaera jaso dezan erabili behar den id-a
     AID = "F0010203040506"
-    
-    led_ireki = LED(17)      
-    
+    LED_PIN = 22
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(LED_PIN, GPIO.OUT)  
+    GPIO.output(LED_PIN, GPIO.LOW)
     #NFC modulua hasieratu
     pn532 = PN532('tty:S0',AID,callbackPN532)
     
@@ -20,8 +24,8 @@ def main():
     pn532.listen()
     
     if len(pn532.device_uid) > 4 and pn532.device_uid[-4:] == "9000" :
-        codepoint = int(pn532.device_uid[:-4], 16)  # "32" → 0x32 (50 in decimal)
-        decoded_char = chr(codepoint)  # 50 → '2'
+        codepoint = int(pn532.device_uid[:-4], 16)  
+        decoded_char = chr(codepoint)  
         id = int(decoded_char)      
         pn532.close()  
         if id != 0:
@@ -43,10 +47,10 @@ def main():
             if erantzuna["baimena"] == "baimenduta":
                 print("Erabiltzailea baimenduta dago")
 
-                led_ireki.on()
+                GPIO.output(LED_PIN, GPIO.HIGH)
                 print("Kutxatila irekiko da")
                 time.sleep(5)
-                led_ireki.off()
+                GPIO.output(LED_PIN, GPIO.LOW)
 
 
             elif erantzuna["baimena"] == "ezbaimenduta":
@@ -59,6 +63,8 @@ def main():
             print("UserID ez zuzena irakurri da (0)")
     else:
         print("Irakurritako mezua ez da zuzena")
+
+    GPIO.cleanup()
 
 
  
