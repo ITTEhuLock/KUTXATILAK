8248856e6fc8df86b85cf3b58efc313dc566c0f3
pmartine082 <pmartinez082@ikasle.ehu.eus>
2025-04-06 15:19:23 +0200

    onCreate button kendu

--- Android/EHULOCK/app/src/main/java/com/example/ehulock/MainActivity.java
+++ Android/EHULOCK/app/src/main/java/com/example/ehulock/MainActivity.java
@@ -9,23 +9,13 @@ import androidx.activity.ComponentActivity;
 
 public class MainActivity extends ComponentActivity {
 
-    Button b1,b2;
+   
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        b1 = findViewById(R.id.button1);
-        b2 = findViewById(R.id.button2);
-
-
-        b1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        
                 Intent intent = new Intent(MainActivity.this, WebviewActivity.class);
-                startActivity(intent);
-            }
-        });
-
+                startActivity(intent);     
 
     }
 
