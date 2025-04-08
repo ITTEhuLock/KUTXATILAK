0b7ec797c2da1816e810ac7796143f338adaa6ce
pmartine082 <pmartinez082@ikasle.ehu.eus>
2025-04-08 13:00:41 +0200

    mapa gehiago konpondu

--- Android/EHULOCK/app/src/main/assets/js/pausoa.js
+++ Android/EHULOCK/app/src/main/assets/js/pausoa.js
@@ -1,6 +1,5 @@
 import {API_URL, gridp6} from './konstanteak.js';
 import { getGelak, getGela } from './gela.js';
-
 import PF from "https://esm.sh/pathfinding";
 
 export const getBideraketa = async () => {
@@ -30,14 +29,17 @@ export async function bideratzeAlgoritmoa(hasiera, amaiera) {
     });
 console.log(grid);
 printGrid(grid);
-// Crear el algoritmo A*
+
+// A* algoritmoa erabili da
 const finder = new PF.AStarFinder();
 
 
 const path = finder.findPath(Math.floor(gela_h[0].x/100*grid.width), Math.floor(gela_h[0].y/100*grid.height), Math.floor(gela_a[0].x/100*grid.width), Math.floor(gela_a[0].y/100*grid.height), grid);
 console.log(Math.floor(gela_h[0].x/100*grid.width), Math.floor(gela_h[0].y/100*grid.height), Math.floor(gela_a[0].x/100*grid.width), Math.floor(gela_a[0].y/100*grid.height))
-// Imprimir el camino encontrado
-console.log("Camino encontrado:", path);
+if(path.length>0)
+console.log("Ibilbidea aurkitu da");
+else
+console.log("Ibilbiderik ez da aurkitu");
 
 return path;
 }
@@ -53,7 +55,7 @@ function printGrid(grid) {
     for (let y = 0; y < rows; y++) {
         let rowString = '';
         for (let x = 0; x < cols; x++) {
-            // 1 si el lugar es un obstáculo (no caminable), 0 si es caminable
+            // 1 ==> oztopo, 0 ==> ibilbide posiblea
             rowString += grid.getNodeAt(x, y).walkable ? '0' : '1';
         }
         gridString += rowString + '\n';
@@ -62,29 +64,3 @@ function printGrid(grid) {
     console.log(gridString);
 }
 
-function setArea(grid, startX, startY, width, height, gridSize, booleanoa) {
-    console.log(startX, startY, width, height);
-    // Convertimos los puntos de píxeles a coordenadas de la grilla
-    const startGridX = Math.floor(startX );
-    const startGridY = Math.floor(startY );
-    console.log(`Inicio: ${startGridX}, ${startGridY}`);
-
-    // Calculamos las dimensiones del área en términos de celdas de la grilla
-    const numCols = Math.floor(width);
-    const numRows = Math.floor(height);
-
-    // Marcamos las celdas en el área como no caminables
-    for (let x = startGridX; x < startGridX + numCols; x++) {
-        for (let y = startGridY; y < startGridY + numRows; y++) {
-                    console.log(gridSize);
-                    const nx = Math.floor(x/100*gridSize);
-                    const ny = Math.floor(y/100*gridSize);
-                    console.log(`Marcando la celda ${nx}, ${ny} como ${booleanoa} caminable`);
-                    grid.setWalkableAt(nx, ny, booleanoa);
-        }
-            
-            
-        
-    }
-
-}
