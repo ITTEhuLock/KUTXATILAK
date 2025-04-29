1d425ae5f379c254d179265aa343b71222421618
RenobalesAnder <160117528+RenobalesAnder@users.noreply.github.com>
2025-04-29 18:12:17 +0200

    Update app.js

--- Android/EHULOCK/app/src/main/assets/js/app.js
+++ Android/EHULOCK/app/src/main/assets/js/app.js
@@ -1,16 +1,20 @@
 export async function loadFooter() {
+
     const footer = document.getElementById('footer');
     const buttonHome = document.createElement('button');
     const buttonBerria = document.createElement('button');
     const buttonIreki = document.createElement('button');
     const buttonMapa = document.createElement('button');
     const buttonProfila = document.createElement('button');
-    
+
+
     const irudiaHome = document.createElement('img');
     const irudiaBerria = document.createElement('img');
     const irudiaIreki = document.createElement('img');
     const irudiaProfila = document.createElement('img');
     const irudiaMapa = document.createElement('img');
+    
+
     irudiaHome.className = 'irudia';
     irudiaBerria.className = 'irudia';
     irudiaIreki.className = 'irudia';
@@ -22,6 +26,7 @@ export async function loadFooter() {
     irudiaIreki.src = '../pics/unlock.svg';
     irudiaProfila.src = '../pics/profila.svg';
     irudiaMapa.src = '../pics/pin.png';
+
     buttonMapa.addEventListener('click', () => {
         window.location.href = './mapak.html';
     });
@@ -37,12 +42,14 @@ export async function loadFooter() {
     buttonProfila.addEventListener('click', () => {
         window.location.href = './profila.html';
     });
+   
     
     buttonHome.appendChild(irudiaHome);
     buttonBerria.appendChild(irudiaBerria);
     buttonIreki.appendChild(irudiaIreki);
     buttonMapa.appendChild(irudiaMapa);
     buttonProfila.appendChild(irudiaProfila);
+    
 
 
    
@@ -53,33 +60,66 @@ export async function loadFooter() {
    footer.appendChild(buttonMapa);
    footer.appendChild(buttonProfila);
    
+   
    return;
 
  
 }
 
 export async function loadHeader(){
+
     const header = document.getElementById('header');
 
     const buttonLogo = document.createElement('button');
     const buttonBirkargatu = document.createElement('button');
     const irudiaLogo = document.createElement('img');
     const irudiaBirkargatu = document.createElement('img');
+
+    const buttonHizkuntza = document.createElement('button');
+    const imgHizkuntza = document.createElement('img');
+
     irudiaLogo.src = '../pics/logo.png';
     irudiaLogo.className = 'irudia';
     buttonLogo.appendChild(irudiaLogo);
+
     irudiaBirkargatu.src = '../pics/birkargatu.svg';
     irudiaBirkargatu.className = 'irudia';
     buttonBirkargatu.appendChild(irudiaBirkargatu);
+
+    imgHizkuntza.src = '../pics/ikurriña.png';
+    imgHizkuntza.alt = 'Hizkuntza';
+    imgHizkuntza.className = 'irudia';
+    buttonHizkuntza.appendChild(imgHizkuntza);
+    
+
+    let idiomaActual = 'eus';
+
+    buttonHizkuntza.addEventListener('click', () => {
+        cambiarIdioma(); 
+    
+        if (idiomaActual === 'eus') {
+            imgHizkuntza.src = '../pics/castellano.png';
+            idiomaActual = 'es';
+        } else {
+            imgHizkuntza.src = '../pics/ikurriña.png';
+            idiomaActual = 'eus';
+        }
+    });
+
     buttonBirkargatu.addEventListener('click', () => {
         window.location.reload();
     });
+
     buttonLogo.addEventListener('click', () => {
         window.location.href = './home.html';
     });
+
+   
+
     header.appendChild(buttonLogo);
     header.appendChild(buttonBirkargatu);
-    
+    header.appendChild(buttonHizkuntza);
+
 }
 
 document.addEventListener('DOMContentLoaded', () => {
